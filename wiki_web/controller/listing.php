1bd3d66ed8c6218f62bde081089cd743e86558ee
徐晶 <xujing@jxulife.com>
2020-05-09 22:49:06 +0800

    存档

---
+++ wiki_web/controller/listing.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * listing
+ *
+ * @namespace app\controller
+ */
+namespace app\controller;
+
+use app\controller\base;
+
+/**
+ * listing
+ */
+class listing extends base
+{
+
+    function doRequest()
+    {
+        return '/listing';
+    }
+}
---
+++ wiki_web/controller/test.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * listing
+ *
+ * @namespace app\controller
+ */
+namespace app\controller;
+
+use app\controller\base;
+
+/**
+ * listing
+ */
+class test extends base
+{
+    
+    function doRequest()
+    {
+        return '/home/test';
+    }
+}
